7730dfe47443dea6bc86797acc100e21768cfdb3
Georgiana Roberta Muresan <Muresan.Gh.Roberta@student.utcluj.ro>
2022-12-13 12:37:32 +0200

    roberta-userprog first commit

--- src/HAL9000/headers/syscall.h
+++ src/HAL9000/headers/syscall.h
@@ -1,21 +1,60 @@
 #pragma once
 
+#include "process_internal.h"
+#include "syscall_defs.h"
+#include "thread_internal.h"
+#include "filesystem.h"
+
+
+typedef struct _PROCESS_HANDLE {
+
+    PPROCESS             Process;
+
+    UM_HANDLE            Handle;
+
+    LIST_ENTRY           ProcessHandleList;
+
+} PROCESS_HANDLE, * PPROCESS_HANDLE;
+
+
+struct _PROCESS_HANDLE_LIST {
+
+    LIST_ENTRY ProcessHandleListHead;
+};
+
+typedef struct _FILE_HANDLE {
+
+    FILE_OBJECT          File;
+
+    UM_HANDLE            Handle;
+
+    LIST_ENTRY           FileHandleList;
+
+} FILE_HANDLE, * FILE_HANDLE;
+
+
+struct _FILE_HANDLE_LIST {
+
+    LIST_ENTRY FileHandleListHead;
+};
+
+
 void
 SyscallPreinitSystem(
     void
-    );
+);
 
 STATUS
 SyscallInitSystem(
     void
-    );
+);
 
 STATUS
 SyscallUninitSystem(
     void
-    );
+);
 
 void
 SyscallCpuInit(
     void
-    );
+);
--- src/HAL9000/src/syscall.c
+++ src/HAL9000/src/syscall.c
@@ -7,15 +7,24 @@
 #include "mmu.h"
 #include "process_internal.h"
 #include "dmp_cpu.h"
+#include "io.h"
+#include "iomu.h"
+#include "cl_string.h"
+#include "process.h"
 
 extern void SyscallEntry();
 
 #define SYSCALL_IF_VERSION_KM       SYSCALL_IMPLEMENTED_IF_VERSION
+#define APP_TAG 'PPA'
+#define APP_NEW_TAG 'PPAN'
+
+static struct _PROCESS_HANDLE_LIST m_processHandleList;
+static struct _FILE_HANDLE_LIST m_fileHandleList;
 
 void
 SyscallHandler(
-    INOUT   COMPLETE_PROCESSOR_STATE    *CompleteProcessorState
-    )
+    INOUT   COMPLETE_PROCESSOR_STATE* CompleteProcessorState
+)
 {
     SYSCALL_ID sysCallId;
     PQWORD pSyscallParameters;
@@ -67,7 +76,42 @@ SyscallHandler(
         case SyscallIdIdentifyVersion:
             status = SyscallValidateInterface((SYSCALL_IF_VERSION)*pSyscallParameters);
             break;
-        // STUDENT TODO: implement the rest of the syscalls
+            // STUDENT TODO: implement the rest of the syscalls
+        case SyscallIdProcessExit:
+            status = SyscallProcessExit((STATUS)*pSyscallParameters);
+            break;
+        case SyscallIdProcessCreate:
+            status = SyscallProcessCreate((char*)pSyscallParameters[0],
+                (QWORD)pSyscallParameters[1], (char*)pSyscallParameters[2],
+                (QWORD)pSyscallParameters[3], (UM_HANDLE*)pSyscallParameters[4]);
+            break;
+        case SyscallIdProcessGetPid:
+            status = SyscallProcessGetPid((UM_HANDLE)pSyscallParameters[0], (PID*)pSyscallParameters[1]);
+            break;
+        case SyscallIdProcessWaitForTermination:
+            status = SyscallProcessWaitForTermination((UM_HANDLE)pSyscallParameters[0], (STATUS*)pSyscallParameters[1]);
+            break;
+        case SyscallIdProcessCloseHandle:
+            status = SyscallProcessCloseHandle((UM_HANDLE)*pSyscallParameters);
+            break;
+        case SyscallIdFileCreate:
+            status = SyscallFileCreate((char*)pSyscallParameters[0],
+                (QWORD)pSyscallParameters[1], (BOOLEAN)pSyscallParameters[2],
+                (BOOLEAN)pSyscallParameters[3], (UM_HANDLE*)pSyscallParameters[4]);
+            break;
+        case SyscallIdFileClose:
+            status = SyscallFileClose((UM_HANDLE)*pSyscallParameters);
+            break;
+        case SyscallIdFileRead:
+            status = SyscallFileRead((UM_HANDLE)pSyscallParameters[0],
+                (PVOID)pSyscallParameters[1], (QWORD)pSyscallParameters[2],
+                (QWORD*)pSyscallParameters[3]);
+            break;
+        case SyscallIdFileWrite:
+            status = SyscallFileWrite((UM_HANDLE)pSyscallParameters[0],
+                (PVOID)pSyscallParameters[1], (QWORD)pSyscallParameters[2],
+                (QWORD*)pSyscallParameters[3]);
+            break;
         default:
             LOG_ERROR("Unimplemented syscall called from User-space!\n");
             status = STATUS_UNSUPPORTED;
@@ -88,15 +132,17 @@ SyscallHandler(
 void
 SyscallPreinitSystem(
     void
-    )
+)
 {
-
+    // initializam lista de ProcessHandle
+    InitializeListHead(&m_processHandleList.ProcessHandleListHead);
+    InitializeListHead(&m_fileHandleList.FileHandleListHead);
 }
 
 STATUS
 SyscallInitSystem(
     void
-    )
+)
 {
     return STATUS_SUCCESS;
 }
@@ -104,7 +150,7 @@ SyscallInitSystem(
 STATUS
 SyscallUninitSystem(
     void
-    )
+)
 {
     return STATUS_SUCCESS;
 }
@@ -112,7 +158,7 @@ SyscallUninitSystem(
 void
 SyscallCpuInit(
     void
-    )
+)
 {
     IA32_STAR_MSR_DATA starMsr;
     WORD kmCsSelector;
@@ -129,9 +175,9 @@ SyscallCpuInit(
     ASSERT(umCsSelector + 0x10 == GdtMuGetCS64Usermode());
 
     // Syscall RIP <- IA32_LSTAR
-    __writemsr(IA32_LSTAR, (QWORD) SyscallEntry);
+    __writemsr(IA32_LSTAR, (QWORD)SyscallEntry);
 
-    LOG_TRACE_USERMODE("Successfully set LSTAR to 0x%X\n", (QWORD) SyscallEntry);
+    LOG_TRACE_USERMODE("Successfully set LSTAR to 0x%X\n", (QWORD)SyscallEntry);
 
     // Syscall RFLAGS <- RFLAGS & ~(IA32_FMASK)
     __writemsr(IA32_FMASK, RFLAGS_INTERRUPT_FLAG_BIT);
@@ -170,3 +216,199 @@ SyscallValidateInterface(
 }
 
 // STUDENT TODO: implement the rest of the syscalls
+// SyscallIdProcessExit
+STATUS
+SyscallProcessExit(
+    IN      STATUS                  ExitStatus
+)
+{
+    PPROCESS process = GetCurrentProcess();
+    process->TerminationStatus = ExitStatus;
+    ProcessTerminate(process);
+    return STATUS_SUCCESS;
+}
+
+// SyscallIdProcessCreate
+STATUS
+SyscallProcessCreate(
+    IN_READS_Z(PathLength)
+    char* ProcessPath,
+    IN          QWORD               PathLength,
+    IN_READS_OPT_Z(ArgLength)
+    char* Arguments,
+    IN          QWORD               ArgLength,
+    OUT         UM_HANDLE* ProcessHandle
+)
+{
+    UNREFERENCED_PARAMETER(ProcessPath);
+    UNREFERENCED_PARAMETER(PathLength);
+    UNREFERENCED_PARAMETER(Arguments);
+    UNREFERENCED_PARAMETER(ArgLength);
+    UNREFERENCED_PARAMETER(ProcessHandle);
+    /*
+    PPROCESS Process;
+    STATUS status;
+
+    if (ProcessPath == NULL || ProcessHandle == NULL) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    if (MmuIsBufferValid((char*)ProcessPath, sizeof(char) * PathLength, PAGE_RIGHTS_READ, GetCurrentProcess()) != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    if (MmuIsBufferValid((UM_HANDLE*)ProcessHandle, sizeof(UM_HANDLE), PAGE_RIGHTS_READWRITE, GetCurrentProcess()) != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+    if (ArgLength != 0) {
+        if (Arguments == NULL) {
+            return STATUS_UNSUCCESSFUL;
+        }
+        if (MmuIsBufferValid((char*)Arguments, sizeof(char) * ArgLength, PAGE_RIGHTS_READ, GetCurrentProcess()) != STATUS_SUCCESS) {
+            return STATUS_UNSUCCESSFUL;
+        }
+        if (cl_strlen(Arguments) + 1 != ArgLength) {
+            return STATUS_UNSUCCESSFUL;
+        }
+    }
+    if (cl_strlen(ProcessPath) + 1 != PathLength) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    */
+
+    return STATUS_SUCCESS;
+}
+
+// SyscallIdProcessGetPid
+STATUS
+SyscallProcessGetPid(
+    IN_OPT  UM_HANDLE               ProcessHandle,
+    OUT     PID* ProcessId
+)
+{
+    UNREFERENCED_PARAMETER(ProcessHandle);
+    UNREFERENCED_PARAMETER(ProcessId);
+    return STATUS_SUCCESS;
+}
+
+// SyscallIdProcessWaitForTermination
+STATUS
+SyscallProcessWaitForTermination(
+    IN      UM_HANDLE               ProcessHandle,
+    OUT     STATUS* TerminationStatus
+)
+{
+    UNREFERENCED_PARAMETER(ProcessHandle);
+    UNREFERENCED_PARAMETER(TerminationStatus);
+    return STATUS_SUCCESS;
+}
+
+// SyscallIdProcessCloseHandle
+STATUS
+SyscallProcessCloseHandle(
+    IN      UM_HANDLE               ProcessHandle
+)
+{
+    UNREFERENCED_PARAMETER(ProcessHandle);
+    return STATUS_SUCCESS;
+}
+
+// SyscallIdFileCreate
+STATUS
+SyscallFileCreate(
+    IN_READS_Z(PathLength)
+    char* Path,
+    IN          QWORD                   PathLength,
+    IN          BOOLEAN                 Directory,
+    IN          BOOLEAN                 Create,
+    OUT         UM_HANDLE* FileHandle
+)
+{
+    UNREFERENCED_PARAMETER(Path);
+    UNREFERENCED_PARAMETER(PathLength);
+    UNREFERENCED_PARAMETER(Directory);
+    UNREFERENCED_PARAMETER(Create);
+    UNREFERENCED_PARAMETER(FileHandle);
+    return STATUS_SUCCESS;
+}
+
+// SyscallIdFileClose
+STATUS
+SyscallFileClose(
+    IN          UM_HANDLE               FileHandle
+)
+{
+    UNREFERENCED_PARAMETER(FileHandle);
+    return STATUS_SUCCESS;
+}
+
+// SyscallIdFileRead
+STATUS
+SyscallFileRead(
+    IN  UM_HANDLE                   FileHandle,
+    OUT_WRITES_BYTES(BytesToRead)
+    PVOID                       Buffer,
+    IN  QWORD                       BytesToRead,
+    OUT QWORD* BytesRead
+)
+{
+    UNREFERENCED_PARAMETER(FileHandle);
+    UNREFERENCED_PARAMETER(Buffer);
+    UNREFERENCED_PARAMETER(BytesToRead);
+    UNREFERENCED_PARAMETER(BytesRead);
+
+    //STATUS status;
+    /*
+    if (BytesRead == NULL) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    if (BytesToRead == 0) {
+        *BytesRead = 0;
+        return STATUS_SUCCESS;
+    }
+
+    if (MmuIsBufferValid(
+        Buffer,
+        BytesToRead,
+        PAGE_RIGHTS_READ,
+        GetCurrentProcess()
+    ) != STATUS_SUCCESS) {
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    //status = IoReadFile(FileObject, BytesToRead, NULL, Buffer, BytesRead);
+    */
+
+    return STATUS_SUCCESS;
+}
+
+// SyscallIdFileWrite
+STATUS
+SyscallFileWrite(
+    IN  UM_HANDLE                   FileHandle,
+    IN_READS_BYTES(BytesToWrite)
+    PVOID                       Buffer,
+    IN  QWORD                       BytesToWrite,
+    OUT QWORD* BytesWritten
+)
+{
+    UNREFERENCED_PARAMETER(FileHandle);
+    UNREFERENCED_PARAMETER(BytesWritten);
+    REFERENCED_PARAMETER(Buffer);
+    REFERENCED_PARAMETER(BytesToWrite);
+    /*
+    MmuIsBufferValid(
+        Buffer,
+        BytesToWrite,
+        PAGE_RIGHTS_WRITE,
+        GetCurrentProcess()
+    );
+
+    if (FileHandle == UM_FILE_HANDLE_STDOUT) {
+        LOG("[%s]:[%s]", ProcessGetName(NULL), Buffer);
+    }
+
+    *BytesWritten = BytesToWrite;
+    */
+    return STATUS_SUCCESS;
+}
